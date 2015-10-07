716a175535dca38df0360b9a3e88315abf5c3afa
ideacircle <kg@ideacircle.co.uk>
2015-10-07 13:37:00 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,9 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var xhr = new XMLHttpRequest();
+xhr.open("GET", "192.168.0.7/arduino/digital/13/1", false);
+xhr.send();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
